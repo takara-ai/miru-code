461c5517d5492fdf8c51ab3fd8125caefa1f0636
Jordan Legg <149483255+404missinglink@users.noreply.github.com>
2026-07-13 11:49:43 +0100

    feat(cli): set process.title to miru

--- src/cli.ts
+++ src/cli.ts
@@ -44,6 +44,8 @@ import {
 } from "./utils.ts";
 import { maybeNotifyUpdate, miruVersion } from "./version.ts";
 
+process.title = "miru";
+
 await loadEnvFiles();
 normalizeTakaraApiKeyEnv();
 await loadStoredCredentials();
